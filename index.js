c74da7efb9b8fdf456595ecd81286def3900c13a
eric ng <naclericny@gmail.com>
2024-08-12 21:02:54 -0400

    added timer

--- index.js
+++ index.js
@@ -10,17 +10,32 @@ function wrapLettersInSpan(str) {
     return result;
 }
 
-function main() {
-    fetch("redditpost.json")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+async function foo() {
+    return fetch("redditpost.json")
+        .then(response => response.json());
+}
 
+async function main() {
+    let redditPostsResponse = await foo();
+    let randomInt = Math.floor(Math.random() * redditPostsResponse.length);
+
+    let sentence = redditPostsResponse[randomInt].text
 
     let count = 0
-    let sentence = "The jars collect bodies (and souls probably) to dump at the roots of the trees to feed the trees"
     let total_words = sentence.split(' ').filter(word => word.length > 0).length
+
+    let timeLeft = 5
+    let timerEl = document.getElementById("timer")
+    setInterval(() => {
+        // Decrement the time left
+        timeLeft--;
+
+        // Update the countdown display
+        timerEl.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            timerEl.textContent = "Done";
+        }
+    }, 1000)
 
     let game = document.getElementById("game")
     game.innerHTML = wrapLettersInSpan(sentence)
@@ -30,13 +45,12 @@ function main() {
 
     document.addEventListener('keydown', function (event) {
         let key = event.key
-
+        console.log(count)
         // Regular expression to detect letters, numbers, punctuation, and special characters
         if (/^[a-zA-Z0-9!@#$%^&*()e_+\-=\[\]{};':"\\|,.<>\/?~` ]$/.test(key)) {
 
             if (letterNode[count].innerHTML === " " && key !== " ") {
                 // letterNode[count].className = "letter-correct"
-                console.log("pog")
                 letterNode[count].className = "letter-block-wrong"
             } else if (key === letterNode[count].innerHTML) {
                 letterNode[count].className = "letter-correct"
@@ -57,7 +71,6 @@ function main() {
 
         letterNode[count].classList.add("letter-block")
     });
-
 }
 
 main()
