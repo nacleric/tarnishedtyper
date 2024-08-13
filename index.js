b1417fb4a7b082683bcf8eaf1c73fff55c6c024a
eric ng <naclericny@gmail.com>
2024-08-12 22:27:32 -0400

    WIP restart game

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function wrapLettersInSpan(str) {
-    let result = '';
+    let result = "";
     for (let i = 0; i < str.length; i++) {
         if (str[i] === " ") {
             result += `<span class="space">${str[i]}</span>`;
@@ -10,34 +10,47 @@ function wrapLettersInSpan(str) {
     return result;
 }
 
-async function foo() {
+async function fetchData() {
     return fetch("redditpost.json")
         .then(response => response.json());
 }
 
-async function main() {
-    let redditPostsResponse = await foo();
-    let randomInt = Math.floor(Math.random() * redditPostsResponse.length);
+function newSentence(redditPosts) {
+    let randomInt = Math.floor(Math.random() * redditPosts.length);
+    return redditPosts[randomInt].text
+}
 
-    let sentence = redditPostsResponse[randomInt].text
+function renderGame(sentence, htmlElement) {
+    htmlElement.innerHTML = wrapLettersInSpan(sentence)
+    let letterNode = game.childNodes
+    letterNode[count].classList.add("starting-letter-block")
+}
+
+async function main() {
+    let redditPostsResponse = await fetchData();
+    let sentence = newSentence(redditPostsResponse)
 
     let count = 0
     let total_words = sentence.split(' ').filter(word => word.length > 0).length
 
     let timeLeft = 5
     let timerEl = document.getElementById("timer")
-    setInterval(() => {
-        // Decrement the time left
-        timeLeft--;
 
-        // Update the countdown display
+    let game = document.getElementById("game")
+    let states = {
+        PAUSE: "PAUSE",
+        INPROGRESS: "INPROGRESS",
+    }
+    let currentState = states.PAUSE
+
+    setInterval(() => {
+        timeLeft--;
         timerEl.textContent = timeLeft;
         if (timeLeft <= 0) {
             timerEl.textContent = "Done";
+            currentState = states.PAUSE
         }
     }, 1000)
-
-    let game = document.getElementById("game")
     game.innerHTML = wrapLettersInSpan(sentence)
 
     let letterNode = game.childNodes
@@ -45,31 +58,38 @@ async function main() {
 
     document.addEventListener('keydown', function (event) {
         let key = event.key
-        console.log(count)
-        // Regular expression to detect letters, numbers, punctuation, and special characters
-        if (/^[a-zA-Z0-9!@#$%^&*()e_+\-=\[\]{};':"\\|,.<>\/?~` ]$/.test(key)) {
-
-            if (letterNode[count].innerHTML === " " && key !== " ") {
-                // letterNode[count].className = "letter-correct"
-                letterNode[count].className = "letter-block-wrong"
-            } else if (key === letterNode[count].innerHTML) {
-                letterNode[count].className = "letter-correct"
-            } else {
-                letterNode[count].className = "letter-wrong"
+        if (timeLeft > 0 && currentState === states.PAUSE) {
+            currentState = states.INPROGRESS
+        }
+        if (currentState === states.INPROGRESS) {
+            // Regular expression to detect letters, numbers, punctuation, and special characters
+            if (/^[a-zA-Z0-9!@#$%^&*()e_+\-=\[\]{};':"\\|,.<>\/?~` ]$/.test(key)) {
+                if (letterNode[count].innerHTML === " " && key !== " ") {
+                    letterNode[count].className = "letter-block-wrong"
+                } else if (key === letterNode[count].innerHTML) {
+                    letterNode[count].className = "letter-correct"
+                } else {
+                    letterNode[count].className = "letter-wrong"
+                }
+                count += 1
             }
-            count += 1
+
+            // detect backspace
+            if (event.keyCode === 8) {
+                if (count !== 0) {
+                    count -= 1
+                }
+                letterNode[count].className = ""
+                letterNode[count + 1].classList.remove("letter-block")
+            }
+            letterNode[count].classList.add("letter-block")
         }
 
-        // detect backspace
-        if (event.keyCode === 8) {
-            if (count !== 0) {
-                count -= 1
-            }
-            letterNode[count].className = ""
-            letterNode[count + 1].classList.remove("letter-block")
+        if (currentState === states.PAUSE && event.keyCode === 13) {
+            timeLeft = 5
+            sentence = newSentence(redditPostsResponse)
+            renderGame(sentence, game)
         }
-
-        letterNode[count].classList.add("letter-block")
     });
 }
 
